3c1b418d608a5b19ad368f8a4c7b2c73f69abb3f
chloe2781 <chloe.nguyen.2708@gmail.com>
2023-04-24 12:55:49 -0400

    Clean up code and debug statements

--- population_dynamics/run.cpp
+++ population_dynamics/run.cpp
@@ -2,40 +2,9 @@
 
 int main() {
 
-// BRIAN'S CODE
-//
-//  // initialize random
-//  srand(1337);
-//  Species_Community communities[TOTAL_COMMUNITIES];
-//
-//  for (int community_id = 0; community_id < TOTAL_COMMUNITIES; community_id++){
-//    communities[community_id].population = randRange(MAX_STARTING_POPULATION) + 5;
-//    communities[community_id].region_of_world = randRange(NUM_REGIONS);
-//    communities[community_id].species_type = randRange(NUM_SPECIES);
-//    communities[community_id].growth_rate = rand01();
-//  }
-//
-//  for (int community_id = 0; community_id < TOTAL_COMMUNITIES; community_id++){
-//    std::cout << "ID[" << community_id << "]: of type [" << communities[community_id].species_type <<
-//                 "]: in region [" << communities[community_id].region_of_world << "]: had initial population [" <<
-//                 communities[community_id].population << "]" << std::endl;
-//  }
-//
-//  // run the simulation
-//  population_dynamics(communities);
-//
-//  // print the final populations
-//  std::cout << "\n---------\n---------\n";
-//  for (int community_id = 0; community_id < TOTAL_COMMUNITIES; community_id++){
-//    std::cout << "ID[" << community_id << "]: of type [" << communities[community_id].species_type <<
-//                 "]: in region [" << communities[community_id].region_of_world << "]: had final population [" <<
-//                 communities[community_id].population << "]" << std::endl;
-//  }
-
     // Initialize the random number generator
-    std::cout << " Checkpoint 0 " << std::endl;
+//    std::cout << " Checkpoint 0 " << std::endl;
     std::srand(std::time(0));
-    // Seed the random number generator with current time
 
     // Setting up game config which can account for different factors of simulation run
     GameConfig config;
@@ -49,12 +18,12 @@ int main() {
     config.vaccination = false;
     config.social_distancing = false;
 
-    std::cout << " Checkpoint 0.1 " << std::endl;
+//    std::cout << " Checkpoint 0.1 " << std::endl;
 
     Person people[config.start_population];
 
     // ... code to initialize people ...
-    std::cout << " Checkpoint 1 " << std::endl;
+//    std::cout << " Checkpoint 1 " << std::endl;
 
     // Initialize the people array
     for (int i = 0; i < config.start_population; i++) {
@@ -68,7 +37,7 @@ int main() {
         people[i].immunity = 0;                // set initial immunity as 0 (not immune)
     }
 
-    std::cout << " Checkpoint 2 " << std::endl;
+//    std::cout << " Checkpoint 2 " << std::endl;
 
     // Initialize the variants array
     Variant variants[100];
@@ -81,66 +50,67 @@ int main() {
     variants[0].mutation_rate = 0.01;
     variants[0].immunity = 90;
 
-//    for (int i = 0; i < 2; i++) {
-//            std::cout << "Variant " << i  << " - Variant Number: " << variants[i].variant_num << ", Recovery Time: " << variants[i].recovery_time
-//                            << ", Mortality Rate: " << variants[i].mortality_rate
-//                           << ", Infection Rate: " << variants[i].infection_rate
-//                            << ", Infection Range: " << variants[i].infection_range << ", Mutation Rate: " << variants[i].mutation_rate
-//                            << ", Immunity: " << variants[i].immunity
-//    << std::endl;
-//        }
-
     // Generate random indexes for people to be infected
-    //BUG: before this was calling randRangePos(config.starting_infected)
-    //so we were never getting a random number based on the entire pop
-    //but only on starting_infected. now calling on entire pop
     for (int i = 0; i < config.starting_infected; i++) {
         int id = randRangePos(config.start_population);
+        std::cout << "Person " << id << " is infected" << std::endl;
         people[id].infected = true;
         people[id].day_infected = 0;
         people[id].variant = 0;
     }
 
-    std::cout << " Checkpoint 3" << std::endl;
+//    std::cout << " Checkpoint 3" << std::endl;
 
     std::cout << " ----------------------------------------- " << std::endl;
     std::cout << " Starting Population" << std::endl;
 
     for (int i = 0; i < config.start_population; i++) {
-        std::cout << "Person " << i << " - ID: " << people[i].id << ", X: " << people[i].x
-                << ", Y: " << people[i].y << ", Dead: " << (people[i].dead ? "Yes" : "No")
-                << ", Immunity: " << people[i].immunity << ", infected: " << (people[i].infected ? "Yes" : "No")
-                << ", Day Infected: " << people[i].day_infected << std::endl;
+        std::cout << "Person " << i
+                  << " - ID: " << people[i].id
+                  << ", X: " << people[i].x
+                  << ", Y: " << people[i].y
+                  << ", Dead: " << (people[i].dead ? "Yes" : "No")
+                  << ", Immunity: " << people[i].immunity
+                  << ", infected: " << (people[i].infected ? "Yes" : "No")
+                  << ", Day Infected: " << people[i].day_infected << std::endl;
     }
 
-    std::cout << " Checkpoint 4 " << std::endl;
+//    std::cout << " Checkpoint 4 " << std::endl;
 
     // simulate
     int max_var = disease_simulation(people, variants, config.end_day);
 
-    std::cout << " Checkpoint 5 " << std::endl;
+//    std::cout << " Checkpoint 5 " << std::endl;
 
     // ... code to do something with the updated people ...
     std::cout << " ----------------------------------------- " << std::endl;
 
     std::cout << " Ending Population" << std::endl;
 
+    // print the people after the simulation
     for (int i = 0; i < config.start_population; i++) {
-        std::cout << "Person " << i << " - ID: " << people[i].id << ", X: " << people[i].x
-                        << ", Y: " << people[i].y << ", Dead: " << (people[i].dead ? "Yes" : "No")
-                        << ", Immunity: " << people[i].immunity << ", infected: " << (people[i].infected ? "Yes" : "No")
-                        << ", Day Infected: " << people[i].day_infected << std::endl;
+        std::cout << "Person " << i
+                  << " - ID: " << people[i].id
+                  << ", X: " << people[i].x
+                  << ", Y: " << people[i].y
+                  << ", Dead: " << (people[i].dead ? "Yes" : "No")
+                  << ", Immunity: " << people[i].immunity
+                  << ", infected: " << (people[i].infected ? "Yes" : "No")
+                  << ", Day Infected: " << people[i].day_infected << std::endl;
     }
 
     // print the variants
     std::cout << " ----------------------------------------- " << std::endl;
     for (int i = 0; i < max_var+1; i++) {
-        std::cout << "Variant " << i  << " - Variant Number: " << variants[i].variant_num << ", Recovery Time: " << variants[i].recovery_time
-                        << ", Mortality Rate: " << variants[i].mortality_rate
-                       << ", Infection Rate: " << variants[i].infection_rate
-                        << ", Infection Range: " << variants[i].infection_range << ", Mutation Rate: " << variants[i].mutation_rate
-                        << ", Immunity: " << variants[i].immunity
-<< std::endl;
+        std::cout << "Variant " << i
+                  << " - Variant Number: " << variants[i].variant_num
+                  << ", Recovery Time: " << variants[i].recovery_time
+                  << ", Mortality Rate: " << variants[i].mortality_rate
+                  << ", Infection Rate: " << variants[i].infection_rate
+                  << ", Infection Range: " << variants[i].infection_range
+                  << ", Mutation Rate: " << variants[i].mutation_rate
+                  << ", Immunity: " << variants[i].immunity
+                  << std::endl;
     }
 
     return 0;
--- population_dynamics/util.h
+++ population_dynamics/util.h
@@ -1,7 +1,6 @@
 #include "helpers.h"
 #include <thread>
 #include <atomic>
-// I suggest testing with this set to 1 first!
 #define MAX_THREADS 8 // subject to change
 
 std::atomic<int> max_variant(0);
@@ -12,17 +11,10 @@ std::atomic<int> max_variant(0);
 //create a flat vector to store the board and all the people in each cell
  std::vector<std::vector<Person>> board(BOARD_WIDTH * BOARD_LENGTH);
 
-////clear before every move (won't happen do anything on first run, but that's ok)
-//void clear_board(std::vector<std::vector<std::pair<int, int>>>& board, int start, int end){
-//    for (int i = start; i < end; i++){
-//        board[i].clear();
-//    }
-//}
-
-// function to move ONE person within a fixed distance
-// randomly move on both axes
-// edges are wrapped based on world size
-// SHOULD WORK -- NEW: edited to work with threads
+/* function to move ONE person within a fixed distance
+   randomly move on both axes
+   edges are wrapped based on world size
+*/
 void move(Person *people, int start, int end) {
     //for (int i = 0; i < MAX_STARTING_POPULATION; i++) { //removed to thread
 
@@ -49,45 +41,11 @@ void move(Person *people, int start, int end) {
     }
 }
 
-// ---------------------------------
-
-//COMMENTED OUT BELOW bc wasn't used and we can just check in each function that needs it
-// we would have to call this on every thread anyway, so i don't think it would help like i thought
-
-//function to get all the currently infected people
-// for all people, check if they are currently infected
-// store a list tuples of their positions (x,y)
-// want to calculate only once per time period so doesn't need to be parallel?
-// get after people move, so we have their current coordinates
-//std::list<std::tuple<int, int>> get_infected(Person* people) {
-//    //list<tuple<int, int>> infected;
-//    //for (int i = 0; i < TOTAL_POPULATION; i++){
-//    //    if (people[i].infected == true){
-//    //        infected.push_back(make_tuple(people[i].x, people[i].y));
-//    //}
-//    //return infected;
-//
-//    std::list<std::tuple<int, int>> infected;
-//
-//    for (int i = 0; i < MAX_STARTING_POPULATION; i++) {
-//        if (people[i].infected) { // Check if person is currently infected
-//            infected.push_back(std::make_tuple(people[i].x.load(), people[i].y.load())); // Add position (x, y) as tuple
-//        }
-//    }
-//
-//    return infected;
-//}
-
-// ---------------------------------
-
-// function that simulates ONE infected person dying based on variant mortality rate
-// generate a random val btw 0 and 1 and compare to mortality rate
-//also need to consider if we gain immunity??
-
-//Those who survive a disease long enough gain immunity.
-//Immunity is temporary dictated by virus stats
-// NEW: edited to work with threads
-
+/* Function to simulates ONE infected person dying based on variant mortality rate
+     Generate a random val btw 0 and 1 and compare to mortality rate
+     Those who survive a disease long enough gain immunity.
+     Immunity is temporary dictated by virus stats
+*/
 void die(Person *people, Variant *variants, int start, int end, int curr_day) {
     //for (int i = 0; i < MAX_STARTING_POPULATION; i++) { //removed to thread
     for (int i = start; i < end; i++) {
@@ -111,8 +69,9 @@ void die(Person *people, Variant *variants, int start, int end, int curr_day) {
     }
 }
 
-// Mutations are kept within the *variants list
-// If a new mutation is made, it is modified from its parent variant and added into the list of mutations
+/* Mutations are kept within the *variants list
+    If a new mutation is made, it is modified from its parent variant and added into the list of mutations
+*/
 int mutate(Variant *variants, int i){
     max_variant += 1;
     Variant &new_variant = variants[max_variant];
@@ -122,17 +81,16 @@ int mutate(Variant *variants, int i){
     new_variant.infection_rate = addPossibleVariation(variants[i].infection_rate);
     new_variant.mutation_rate = addPossibleVariation(variants[i].mutation_rate);
     new_variant.immunity = addPossibleVariationInt(variants[i].immunity);
-//    variants[max_variant] = new_variant;
     return max_variant;
 }
 
-//function for ONE person to infect others within infection radius
-
-//for all people, check if they are within infection radius of infected person
-//if they are, generate a random val btw 0 and 1 and compare to infection rate
-//if random val is less than infection rate, infect person
-//if person is already infected, do nothing
-//if person is already dead, do nothing
+/* function for ONE person to infect others within infection radius
+    for all people, check if they are within infection radius of infected person
+    if they are, generate a random val btw 0 and 1 and compare to infection rate
+    if random val is less than infection rate, infect person
+    if person is already infected, do nothing
+    if person is already dead, do nothing
+*/
 void infect(Person *people, Variant *variants, int start, int end, int curr_day){
 
     for (int i = start; i < end; i++) {
@@ -166,8 +124,9 @@ void infect(Person *people, Variant *variants, int start, int end, int curr_day)
 }
 
 
-// Updates the population for all people
-// people will move, die, and infect others if still alive
+/* Updates the population for all people
+    people will move, die, and infect others if still alive
+*/
 void update_all_people(Person *people, Variant *variants, int curr_day) {
 
     std::thread t[MAX_THREADS];
@@ -216,143 +175,40 @@ int disease_simulation(Person *people, Variant *variants, int end_day){ //end da
   return max_var;
 }
 
+// ------------------------------------------------------------------------------------------
 
-// -------------- HW 1 CODE ----------------
+//COMMENTED OUT BELOW bc wasn't used and we can just check in each function that needs it
+// we would have to call this on every thread anyway, so i don't think it would help like i thought
 
-//// function to simulate population change for one community of one species
-////
-//// Note: 1) The change in population for a community is proportional to
-////          its growth_rate and local_population_share
-////       2) If it has collected enough food to feed the population it grows, else it shrinks
-////       3) If the population drops below 5 it goes extinct
-//void update_community_population(Species_Community *communities, int community_id, float local_population_share) {
-//  //
-//  // # TODO: add implementation
-//  //
+//function to get all the currently infected people
+// for all people, check if they are currently infected
+// store a list tuples of their positions (x,y)
+// want to calculate only once per time period so doesn't need to be parallel?
+// get after people move, so we have their current coordinates
+//std::list<std::tuple<int, int>> get_infected(Person* people) {
+//    //list<tuple<int, int>> infected;
+//    //for (int i = 0; i < TOTAL_POPULATION; i++){
+//    //    if (people[i].infected == true){
+//    //        infected.push_back(make_tuple(people[i].x, people[i].y));
+//    //}
+//    //return infected;
 //
-//  // changes in population
-//  float change = communities[community_id].growth_rate * local_population_share * communities[community_id].population;
-//  if (communities[community_id].food_collected >= communities[community_id].population) {
-//    communities[community_id].population += change; // grow
-//  }
-//  else {
-//    communities[community_id].population -= change; //shrink
-//  }
-//  if (communities[community_id].population < 5) {
-//    communities[community_id].population = 0; //extinct
-//  }
-//}
+//    std::list<std::tuple<int, int>> infected;
 //
-//// function to find the local population share for one community of one species
-////
-//// Note: 1) Population share is defined as the percentage of population in a region
-////          that is a given species across all communities of all species
-//float compute_local_population_share(Species_Community *communities, int community_id){
-//  //
-//  // # TODO: add implementation
-//  //
-//  float total_pop = 0;
-//  float species_pop = 0;
-//  for (int i = 0; i < TOTAL_COMMUNITIES; i++) {
-//    // if same region, add to total_pop
-//    if (communities[i].region_of_world == communities[community_id].region_of_world) {
-//      total_pop += communities[i].population;
-//      // if same species in same region, add to species_pop
-//      if (communities[i].species_type == communities[community_id].species_type) {
-//        species_pop += communities[i].population;
-//      }
+//    for (int i = 0; i < MAX_STARTING_POPULATION; i++) {
+//        if (people[i].infected) { // Check if person is currently infected
+//            infected.push_back(std::make_tuple(people[i].x.load(), people[i].y.load())); // Add position (x, y) as tuple
+//        }
 //    }
-//  }
-//  return species_pop/total_pop;
-//}
 //
-//// Helper function for compute_local_population_shares
-//void compute_helper (Species_Community *communities, int start, int end) {
-//  for (int id = start; id < end; id++) {
-//    communities[id].atomic_helper2 = compute_local_population_share(communities, id);
-//  }
-//}
-//
-//// Helper function for update_community_population
-//void compute_helper2 (Species_Community *communities, int start, int end) {
-//  for (int id = start; id < end; id++) {
-//    update_community_population(communities, id, communities[id].atomic_helper2);
-//  }
-//}
-//
-//// Updates the population for all communities of all species
-////
-//// Note: 1) You will want to launch MAX_THREADS to compute this
-////       2) You will need to use compute_local,_population_share and update_community_population
-////       3) Make sure your logic is thread safe! Warning there likely is a data dependency!
-////       4) Feel free to use helper functions if that makes your life easier!
-//void update_all_populations(Species_Community *communities){
-//  //
-//  // # TODO: add implementation
-//  //
-//  std::thread t[MAX_THREADS];
-//  // compute local population shares
-//  for (int i = 0; i < MAX_THREADS; i++) {
-//    int start = i * (TOTAL_COMMUNITIES/MAX_THREADS);
-//    int end = (i+1) * (TOTAL_COMMUNITIES/MAX_THREADS);
-//    t[i] = std::thread(compute_helper, communities, start, end);
-//  }
-//  for (int i = 0; i < MAX_THREADS; i++) {
-//    t[i].join();
-//  }
-//  // update population
-//  for (int i = 0; i < MAX_THREADS; i++) {
-//    int start = i * (TOTAL_COMMUNITIES/MAX_THREADS);
-//    int end = (i+1) * (TOTAL_COMMUNITIES/MAX_THREADS);
-//    t[i] = std::thread(compute_helper2, communities, start, end);
-//  }
-//  for (int i = 0; i < MAX_THREADS; i++) {
-//    t[i].join();
-//  }
-//}
-//
-//// Helper function for food_oracle
-//void compute_helper3 (Species_Community *communities, int start, int end, int id) {
-//  for (int i = start; i < end; i++) {
-//    communities[id].food_collected += food_oracle(i);
-//  }
+//    return infected;
 //}
 
-//// function to simulate food gathering
-////
-//// Note: 1) Each round food starts at 0 and each member of the population tries to collect food
-////       2) Please use food_oracle() to get a new amount of food for each member of the population
-////       3) Please use MAX_THREADS threads per Species_Community! (Not spread across them but for each one)
-////       4) All other implementation details are up to you!
-//void gather_all_food(Species_Community *communities) {
-//  //
-//  // # TODO: add implementation
-//  //
-//  std::thread t[MAX_THREADS];
-//  for (int i = 0; i < TOTAL_COMMUNITIES; i++) {
-//    communities[i].food_collected = 0;
-//    // use MAX_THREADS threads per Species_Community
-//    for (int j = 0; j < MAX_THREADS; j++) {
-//      int start = j * (communities[i].population/MAX_THREADS);
-//      int end = (j+1) * (communities[i].population/MAX_THREADS);
-//      // if last thread, add remainder
-//      if (j == MAX_THREADS-1) {end += communities[i].population % MAX_THREADS; }
-//      t[j] = std::thread(compute_helper3, communities, start, end, i);
+////clear before every move (won't happen do anything on first run, but that's ok)
+//void clear_board(std::vector<std::vector<std::pair<int, int>>>& board, int start, int end){
+//    for (int i = start; i < end; i++){
+//        board[i].clear();
 //    }
-//    for (int j = 0; j < MAX_THREADS; j++) {
-//      t[j].join();
-//    }
-//  }
 //}
 
-//void population_dynamics(Species_Community *communities){
-//  //
-//  // # TODO
-//  //
-//  for (int i = 0; i < NUM_TIME_PERIODS; i++) {
-//    gather_all_food(communities);
-//    update_all_populations(communities);
-//  }
-//}
-
-// -------------- HW 1 CODE ----------------
+// ------------------------------------------------------------------------------------------
